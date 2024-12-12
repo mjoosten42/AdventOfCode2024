0e9cae85b30fc5bb283d0bb6c1f8f675134c106a
maarten <maarten250@gmail.com>
2024-12-12 16:42:32 +0100

    Day 9 part 2

--- 9/part2.py
+++ 9/part2.py
@@ -4,44 +4,68 @@ def point(to, fro):
 
     print(f"{' ' * (first)}^{' ' * (second - first - 1)}^")
 
+def debug(fs, ids):
+    id = 0;
 
-def debug(fs):
+    print(fs)
+    print(ids)
+
     for i, digit in enumerate(fs):
         digit = int(digit)
 
         if i % 2:
             print('.' * digit, end='')
         else:
-            print(str(i // 2) * digit, end='')
+            print(str(ids[i]) * digit, end='')
+
+            if digit > 0:
+                id += 1
 
     print()
 
-fs=[int(x) for x in open(0).read().strip()]
+fs = [int(x) for x in open(0).read().strip()]
+ids = [0 if i % 2 else i // 2 for i, _ in enumerate(fs)]
+id = max(ids)
 
-debug(fs)
-
-fileptr = len(fs) - (1 - len(fs) % 2) - 1
-
-while fileptr >= 0:
+while id >= 0:
     freeptr = 1
+    fileptr = ids.index(id)
     file_size = fs[fileptr]
     
-    while freeptr < len(fs):
+    while freeptr < fileptr:
         free_size = fs[freeptr]
 
         if free_size >= file_size:
-            print(fs)
-            point(freeptr, fileptr)
-
             fs[freeptr] = 0
-        
+            fs.insert(freeptr + 1, file_size)
+            fs.insert(freeptr + 2, free_size - file_size)
+
+            fs[fileptr + 1] += file_size
+            fs[fileptr + 2] = 0
+
+            ids.insert(freeptr + 1, id)
+            ids.insert(freeptr + 2, 0)
+            ids[fileptr + 2] = 0
 
             break
 
         freeptr += 2
-
-    fileptr -= 2
     
+    id -= 1
 
+total = 0
+block = 0
 
-debug(fs)
+for i, digit in enumerate(fs):
+    digit = int(digit)
+
+    if not i % 2:
+        for j in range(block, block + digit):
+            total += j * ids[i]
+
+        if digit > 0:
+            id += 1
+
+    block += digit
+
+print(total)
